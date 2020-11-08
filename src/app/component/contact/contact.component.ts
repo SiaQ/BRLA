b4c5f75733b0b1386e97c58adf23bd94db1a4783
Kuba Gabrysiak <k.gab19952@gmail.com>
2020-11-08 13:35:27 +0100

    feat(app): add pages

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Person} from '../../model/person.model';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  public people: Person[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const user: Person = {
+      firstName: 'Kuba',
+      lastName: 'Gabrysiak'
+    };
 
+    const userTwo: Person = {
+      firstName: 'Adam',
+      lastName: 'Mickiewicz'
+    };
+
+    this.people.push(user);
+    this.people.push(userTwo);
+  }
 }
